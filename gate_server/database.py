c58a11ecd31aff00e7e8cbf615abde0987a11f26
OOM PLS <ordinacija@drslavica.com>
2024-09-13 21:31:21 +0200

    DB fix

--- gate_server/database.py
+++ gate_server/database.py
@@ -53,7 +53,7 @@ def close_db(connection):
 
 
 def add_user(connection, email: str):
-    sql = f"INSERT INTO users (email) VALUES ('{email}',)"
+    sql = f"INSERT INTO users (email) VALUES ('{email}')"
 
     print(f"ADD SQL: {sql}")
     try:
